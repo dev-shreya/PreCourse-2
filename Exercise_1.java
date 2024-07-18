747001e82dd56b69e65d1bb12edc0bf555d697eb
dev-shreya <shreyaabond005@gmail.com>
2024-07-17 20:44:34 -0700

    Done Precourse-2 Binary search

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,27 @@
 class BinarySearch { 
-    // Returns index of x if it is present in arr[l.. r], else return -1 
+    // Returns index of x if it is present in arr[l.. r], else return -1
+    // Time Complexity : O(nlogn)
+// Space Complexity :O(1)
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        l=0;
+        r=arr.length-1;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+//            Now calculating the middle element
+            int midVal=arr[mid];
+            if(midVal<x){
+                l=mid+1;
+            }else if(midVal>x){
+                r=mid-1;
+            }
+            else {
+                return mid;
+            }
+
+        }
+        return -1;
     } 
   
     // Driver method to test above 
@@ -11,7 +30,7 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
+        int x = 3;
         int result = ob.binarySearch(arr, 0, n - 1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
